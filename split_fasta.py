e734db6452a144bb5c7bc0ac09faaec208feb470
Florian Plaza Oñate <florian.plaza@jouy.inra.fr>
2015-08-31 17:03:25 +0200

    Add other SI suffixes (k,M,T)

--- split_fasta.py
+++ split_fasta.py
@@ -37,34 +37,53 @@ def isdir(path):
         raise argparse.ArgumentTypeError(msg)
     return path
 
+def num_with_si_suffix(num):
+    unit_to_value = {
+            'k': 2**10,    # kilo
+            'M': 2**20,    # mega
+            'G': 2**30,    # giga
+            'T': 2**40,   # tera
+            }
+
+    if num[-1].isdigit():
+        value=int(num)
+    else:
+        value=int(num[0:-1])
+        suffix=num[-1]
+        if suffix not in unit_to_value.keys():
+            msg = "unknown suffix '{0}'".format(suffix)
+            raise argparse.ArgumentTypeError(msg)
+        value *= unit_to_value[suffix]
+
+    return value    
 
 def get_parameters():
     parser = argparse.ArgumentParser(description=__doc__,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', '--input-file', dest='fasta_file', required=True,
-                        default=argparse.SUPPRESS, type=isfile,
-                        help='Input FASTA file to split')
+            default=argparse.SUPPRESS, type=isfile,
+            help='Input FASTA file to split')
     parser.add_argument('-n', '--chunks', dest='num_chunks', type=int,
-                        help='Number of chunks')
+            help='Number of chunks')
     parser.add_argument('-m', '--max_file_size', dest='max_file_size',
-                        type=int, help='Instead of a precise number,'
-                        ' indicate an expected file size for each chunk '
-                        '(in GB).')
+            type=num_with_si_suffix, help='Instead of a precise number,'
+            ' indicate an expected file size for each chunk in bytes'
+            '(k, M, G, T suffixes are accepted).')
     parser.add_argument('-o', '--output-dir', dest='output_dir', type=isdir,
-                        default=os.curdir + os.sep, help='Output directory')
+            default=os.curdir + os.sep, help='Output directory')
     return parser.parse_args(), parser
 
 
 def read_fasta(fasta_stream):
     header, seq = None, []
     for line in fasta_stream:
-            line = line.rstrip()
-            if line.startswith(">"):
-                    if header:
-                            yield (header, ''.join(seq))
-                    header, seq = line, []
-            else:
-                    seq.append(line)
+        line = line.rstrip()
+        if line.startswith(">"):
+            if header:
+                yield (header, ''.join(seq))
+            header, seq = line, []
+        else:
+            seq.append(line)
     if header:
         yield (header, ''.join(seq))
 
@@ -83,14 +102,14 @@ def count_entries(fasta_file):
 
 def split(fasta_file, chunk_size, output_dir):
     (fasta_file_basename,
-     fasta_file_extension)  = os.path.splitext(os.path.basename(fasta_file))
+            fasta_file_extension)  = os.path.splitext(os.path.basename(fasta_file))
     output_file_prefix = os.path.join(output_dir, fasta_file_basename)
     try:
         with open(fasta_file, 'rt') as stream:
             cur_chunk = 1
             entries_in_chunk = 0
             chunk_file = "{0}_{1}{2}".format(output_file_prefix, cur_chunk,
-                                             fasta_file_extension)
+                    fasta_file_extension)
             chunk_stream = open(chunk_file, 'wt')
             for header, seq in read_fasta(stream):
                 if (entries_in_chunk == chunk_size):
@@ -98,61 +117,49 @@ def split(fasta_file, chunk_size, output_dir):
                     entries_in_chunk = 0
                     chunk_stream.close()
                     chunk_file = "{0}_{1}{2}".format(output_file_prefix,
-                                                     cur_chunk,
-                                                     fasta_file_extension)
+                            cur_chunk,
+                            fasta_file_extension)
                     chunk_stream = open(chunk_file, 'wt')
                     print("{1}{0}{2}".format(os.linesep, header, fill(seq)),
-                          file=chunk_stream)
+                            file=chunk_stream)
                     entries_in_chunk = entries_in_chunk + 1
                 else:
                     print("{1}{0}{2}".format(os.linesep, header, fill(seq)),
-                          file=chunk_stream)
+                            file=chunk_stream)
                     entries_in_chunk = entries_in_chunk + 1
             chunk_stream.close()
     except IOError:
         sys.exit("Error cannot open {0}".format(fasta_file))
 
-
-def sizeof_fmt(num):
-    for x in ['bytes','KB','MB','GB','TB', 'PB']:
-        if num < 1024.0:
-            return num, x
-        num /= 1024.0
-    raise ValueError('The file size exceed the PB, I stop here')
-
-
 def fill(text, width=80):
     """Split text"""
     return os.linesep.join(text[i:i+width] for i in xrange(0, len(text), width))
 
-
 def split_depending_on_size(fasta_file, max_file_size, output_dir):
     (fasta_file_basename,
-     fasta_file_extension)  = os.path.splitext(os.path.basename(fasta_file))
+            fasta_file_extension)  = os.path.splitext(os.path.basename(fasta_file))
     output_file_prefix = os.path.join(output_dir, fasta_file_basename)
     try:
         with open(fasta_file, 'rt') as stream:
             cur_chunk = 1
             file_size = 0
-            scale = "B"
             chunk_file = "{0}_{1}{2}".format(output_file_prefix, cur_chunk,
-                                             fasta_file_extension)
+                    fasta_file_extension)
             chunk_stream = open(chunk_file, 'wt')
             for header, seq in read_fasta(stream):
-                if (int(math.floor(file_size)) >= max_file_size and scale == "GB"):
+                if (file_size >= max_file_size):
                     cur_chunk = cur_chunk + 1
                     chunk_stream.close()
                     del(chunk_file)
                     file_size = 0
-                    scale = "B"
                     chunk_file = "{0}_{1}{2}".format(output_file_prefix,
-                                                     cur_chunk,
-                                                     fasta_file_extension)
+                            cur_chunk,
+                            fasta_file_extension)
                     chunk_stream = open(chunk_file, 'wt')
-                else:
-                    print("{1}{0}{2}".format(os.linesep, header, fill(seq)),
-                          file=chunk_stream)
-                    file_size, scale = sizeof_fmt(os.stat(chunk_file).st_size)
+
+                output = "{1}{0}{2}{0}".format(os.linesep, header, fill(seq))
+                chunk_stream.write(output)
+                file_size += len(output)
             chunk_stream.close()
     except IOError:
         sys.exit("Error cannot open {0}".format(fasta_file))
@@ -162,7 +169,7 @@ if __name__ == '__main__':
     args, parser = get_parameters()
     if not args.num_chunks and not args.max_file_size:
         print("Please indicate the number of chunks or the max file size to "
-                 "split the fasta file !", file=sys.stderr)
+                "split the fasta file !", file=sys.stderr)
         sys.exit(parser.print_help())
 
     if args.num_chunks:
@@ -171,13 +178,13 @@ if __name__ == '__main__':
         print("{0} has {1} FASTA entries".format(args.fasta_file, num_entries))
         chunk_size = (num_entries + args.num_chunks -1)/(args.num_chunks)
         print("Dividing {0} in {1} chunks of {2} entries".format(args.fasta_file,
-                                                                 args.num_chunks,
-                                                                 chunk_size))
+            args.num_chunks,
+            chunk_size))
         split(args.fasta_file, chunk_size, args.output_dir)
     elif args.max_file_size:
-        print("Start creating chunks of {0} GB".format(args.max_file_size))
+        print("Start creating chunks")
         split_depending_on_size(args.fasta_file, args.max_file_size,
-                                args.output_dir)
-    print("Done")
+                args.output_dir)
+        print("Done")
 
 
